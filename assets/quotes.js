2260b2bde6e2fe31216a6d5ff80fc525b427d194
azazel <60569774+azazel0907@users.noreply.github.com>
2020-05-05 03:28:00 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! maybeybehhhhhhhhh uwwwwwwuuuuuuuu" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "ABIZARYAN", quotes: "sompong maybeybehhhhhh" },
+    { author: "ABIZARYAN", quotes: "sompong & bidong uuuwwwwuuuuuuu" },
+    { author: "ABIZARYAN", quotes: "Hey! maybeybehhhhhhhhh uwwwwwwuuuuuuuu" },
+    { author: "ABIZARYAN", quotes: "I love you sompongku." },
+    { author: "ABIZARYAN", quotes: "I may not with you everyday, but I love you everyday sompong." },
+    { author: "ABIZARYAN", quotes: "I love you lely." },
+    { author: "ABIZARYAN", quotes: "I'm sorry for loving you :)." },
 ];
